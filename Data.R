b0033d78911f677ec8fcd807cd42c87006a2dba2
rnb134 <robert.barkanic@gmail.com>
2018-02-12 15:33:55 -0500

    pull in index and fixed income data

--- Data.R
+++ Data.R
@@ -2,12 +2,16 @@ library(quantmod)
 library(plotly)
 
 stocks <- c("UIS","ACN","IBM","LDOS","SAIC")
+indices <- c("^GSPC","^DJI","^RUT")
+fixedIncome <-c("DGS10","DGS2","T10Y2Y")
 
-sDate <- '2015-01-01'
+sDate <- '2017-01-01'
 eDate <- Sys.Date()-1
 
 
 getSymbols(stocks, src = 'google', from = sDate, to = eDate)
+getSymbols(indices,auto.assign = TRUE, from = sDate)
+getSymbols(fixedIncome, auto.assign = TRUE, src = 'FRED', from = sDate, to = eDate)
 
 
 candleChart(UIS, up.col = "black", dn.col = "red", theme = "white")
